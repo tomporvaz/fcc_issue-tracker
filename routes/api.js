3d2b2a53c33182900474b8f6d944af35fedaf7be
tomporvaz <tomporvaz@gmail.com>
2019-09-22 19:19:02 -0400

    Added connection to database using mongoose.

--- routes/api.js
+++ routes/api.js
@@ -14,12 +14,21 @@ var ObjectId = require('mongodb').ObjectID;
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+mongoose.connect(CONNECTION_STRING || 'mongodb://localhost/exercise-track' )
+//from quick start guide in mongoose docs
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, 'connection error'));
+db.once('open', function (){
+  console.log("DB sucess using mongoose!")
+});
 module.exports = function (app) {
 
-  MongoClient.connect(CONNECTION_STRING, function(err, db) {
+/*   MongoClient.connect(CONNECTION_STRING, function(err, db) {
     if(err){console.error(`DB failed because.... ${err}`)}
     else {console.log(`DB sucess!!!!!`)};
-  });
+  }); */
 
   app.route('/api/issues/:project')
   
