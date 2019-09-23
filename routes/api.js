5b72bce50bdf2140bf4dc34135d64e44ed0f8e6f
tomporvaz <tomporvaz@gmail.com>
2019-09-22 20:43:27 -0400

    Created issue schema and model.  Built issue object in post route, saved to DB, and returned object.

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,8 @@ var ObjectId = require('mongodb').ObjectID;
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 const mongoose = require('mongoose')
+
+module.exports = function (app) {
 const Schema = mongoose.Schema;
 mongoose.connect(CONNECTION_STRING || 'mongodb://localhost/exercise-track' )
 //from quick start guide in mongoose docs
@@ -22,9 +24,22 @@ let db = mongoose.connection;
 db.on("error", console.error.bind(console, 'connection error'));
 db.once('open', function (){
   console.log("DB sucess using mongoose!")
-});
-module.exports = function (app) {
 
+  //issue schema and model
+  const issueSchema = new Schema({
+    projectname: {type: String, required: true},
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_on: {type: Date, required: true},
+    updated_on: {type: Date},
+    created_by: {type: String, required: true},
+    assigned_to: {type: String, required: false},
+    status_text: {type: String, required: false}
+  })
+
+  const Issue = mongoose.model('Issue', issueSchema);
+
+});
 /*   MongoClient.connect(CONNECTION_STRING, function(err, db) {
     if(err){console.error(`DB failed because.... ${err}`)}
     else {console.log(`DB sucess!!!!!`)};
@@ -39,6 +54,25 @@ module.exports = function (app) {
     
     .post(function (req, res){
       var project = req.params.project;
+      const now = new Date();
+      const issue = new Issue({
+        projectname: project,
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_on: now,
+        updated_on: null,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to,
+        status_text: req.body.status_text
+      })
+
+      issue.save(function(err, issueObj){
+        if(err){
+          console.error(`Error saving issue: ${err}`);
+        } else {
+          return res.json(issueObj);
+        }
+      })
       
     })
     
