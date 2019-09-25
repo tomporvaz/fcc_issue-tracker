261fdc0a396444007eff97a5fbb5709601388841
tomporvaz <tomporvaz@gmail.com>
2019-09-24 20:49:52 -0400

    Added missing parantheses and brackets to functional tests.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -80,9 +80,10 @@ suite('Functional Tests', function() {
         assert.isOk(res.body._id, "doesn't have an _id");
         
       });
-      
-      test('Missing required fields', function(done) {
-        chai.request(server)
+    });
+    
+    test('Missing required fields', function(done) {
+      chai.request(server)
       .post('/api/issues/test')
       .send({
         issue_title: '',
@@ -94,70 +95,69 @@ suite('Functional Tests', function() {
         assert.isNotOk(res.body._id, "doesn't have an _id");
         
       });
-        
-      });
-      
-    });
-    
-    suite('PUT /api/issues/{project} => text', function() {
-      
-      test('No body', function(done) {
-        
-      });
-      
-      test('One field to update', function(done) {
-        
-      });
-      
-      test('Multiple fields to update', function(done) {
-        
-      });
-      
-    });
-    
-    suite('GET /api/issues/{project} => Array of objects with issue data', function() {
-      
-      test('No filter', function(done) {
-        chai.request(server)
-        .get('/api/issues/test')
-        .query({})
-        .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.isArray(res.body);
-          assert.property(res.body[0], 'issue_title');
-          assert.property(res.body[0], 'issue_text');
-          assert.property(res.body[0], 'created_on');
-          assert.property(res.body[0], 'updated_on');
-          assert.property(res.body[0], 'created_by');
-          assert.property(res.body[0], 'assigned_to');
-          assert.property(res.body[0], 'open');
-          assert.property(res.body[0], 'status_text');
-          assert.property(res.body[0], '_id');
-          done();
-        });
-      });
-      
-      test('One filter', function(done) {
-        
-      });
-      
-      test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
-        
-      });
-      
-    });
-    
-    suite('DELETE /api/issues/{project} => text', function() {
-      
-      test('No _id', function(done) {
-        
-      });
-      
-      test('Valid _id', function(done) {
-        
-      });
       
     });
     
   });
   
+  suite('PUT /api/issues/{project} => text', function() {
+    
+    test('No body', function(done) {
+      
+    });
+    
+    test('One field to update', function(done) {
+      
+    });
+    
+    test('Multiple fields to update', function(done) {
+      
+    });
+    
+  });
+  
+  suite('GET /api/issues/{project} => Array of objects with issue data', function() {
+    
+    test('No filter', function(done) {
+      chai.request(server)
+      .get('/api/issues/test')
+      .query({})
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.property(res.body[0], 'issue_title');
+        assert.property(res.body[0], 'issue_text');
+        assert.property(res.body[0], 'created_on');
+        assert.property(res.body[0], 'updated_on');
+        assert.property(res.body[0], 'created_by');
+        assert.property(res.body[0], 'assigned_to');
+        assert.property(res.body[0], 'open');
+        assert.property(res.body[0], 'status_text');
+        assert.property(res.body[0], '_id');
+        done();
+      });
+    });
+    
+    test('One filter', function(done) {
+      
+    });
+    
+    test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
+      
+    });
+    
+  });
+  
+  suite('DELETE /api/issues/{project} => text', function() {
+    
+    test('No _id', function(done) {
+      
+    });
+    
+    test('Valid _id', function(done) {
+      
+    });
+    
+  });
+  
+});
