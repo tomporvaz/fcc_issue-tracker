547a26835e2c01c92d7b07f05ed54cf9aba089ac
tomporvaz <tomporvaz@gmail.com>
2019-09-24 20:45:55 -0400

    Added tests for POST Required Fields Filled IN, and Missing Required Fields.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -40,11 +40,11 @@ suite('Functional Tests', function() {
         approximately +/- 2mins (120000ms), 
         or chai-datetime plugin
         */
-       const created_on_date = new Date(res.body.created_on);
-       assert.approximately(created_on_date.getTime(), today.getTime(), 120000, "created_on is not within 2mins of submission");
-       const updated_on_date = new Date(res.body.updated_on); 
-       assert.approximately(updated_on_date.getTime(), today.getTime(), 120000, "updated_on isn't within 2 mins of submission");
-
+        const created_on_date = new Date(res.body.created_on);
+        assert.approximately(created_on_date.getTime(), today.getTime(), 120000, "created_on is not within 2mins of submission");
+        const updated_on_date = new Date(res.body.updated_on); 
+        assert.approximately(updated_on_date.getTime(), today.getTime(), 120000, "updated_on isn't within 2 mins of submission");
+        
         assert.isTrue(res.body.open, "open is not true");
         assert.isOk(res.body._id, "doesn't have an _id");
         
@@ -55,73 +55,109 @@ suite('Functional Tests', function() {
     });
     
     test('Required fields filled in', function(done) {
-      
-    });
-    
-    test('Missing required fields', function(done) {
-      
-    });
-    
-  });
-  
-  suite('PUT /api/issues/{project} => text', function() {
-    
-    test('No body', function(done) {
-      
-    });
-    
-    test('One field to update', function(done) {
-      
-    });
-    
-    test('Multiple fields to update', function(done) {
-      
-    });
-    
-  });
-  
-  suite('GET /api/issues/{project} => Array of objects with issue data', function() {
-    
-    test('No filter', function(done) {
       chai.request(server)
-      .get('/api/issues/test')
-      .query({})
+      .post('/api/issues/test')
+      .send({
+        issue_title: 'Title',
+        issue_text: 'text',
+        created_by: 'Functional Test - Every field filled in'
+      })
       .end(function(err, res){
-        assert.equal(res.status, 200);
-        assert.isArray(res.body);
-        assert.property(res.body[0], 'issue_title');
-        assert.property(res.body[0], 'issue_text');
-        assert.property(res.body[0], 'created_on');
-        assert.property(res.body[0], 'updated_on');
-        assert.property(res.body[0], 'created_by');
-        assert.property(res.body[0], 'assigned_to');
-        assert.property(res.body[0], 'open');
-        assert.property(res.body[0], 'status_text');
-        assert.property(res.body[0], '_id');
-        done();
+        assert.equal(res.status, 200, "response is not 200");
+        assert.equal(res.body.issue_title, 'Title', "Title is not Title");
+        assert.equal(res.body.issue_text, 'text', "text is not text");
+        assert.equal(res.body.created_by, 'Functional Test - Every field filled in', "created_by doesn't match");
+        assert.equal(res.body.assigned_to, '', "assigned_to doesn't match");
+        assert.equal(res.body.status_text, '', "status_text doesn't match");
+        
+        /*Tests for created_on and updated_on */
+        const created_on_date = new Date(res.body.created_on);
+        assert.approximately(created_on_date.getTime(), today.getTime(), 120000, "created_on is not within 2mins of submission");
+        const updated_on_date = new Date(res.body.updated_on); 
+        assert.approximately(updated_on_date.getTime(), today.getTime(), 120000, "updated_on isn't within 2 mins of submission");
+        
+        assert.isTrue(res.body.open, "open is not true");
+        assert.isOk(res.body._id, "doesn't have an _id");
+        
       });
-    });
-    
-    test('One filter', function(done) {
+      
+      test('Missing required fields', function(done) {
+        chai.request(server)
+      .post('/api/issues/test')
+      .send({
+        issue_title: '',
+        issue_text: 'text',
+        created_by: 'Functional Test - Every field filled in'
+      })
+      .end(function(err, res){
+        assert.notEqual(res.status, 200, "response is not 200");
+        assert.isNotOk(res.body._id, "doesn't have an _id");
+        
+      });
+        
+      });
       
     });
     
-    test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
+    suite('PUT /api/issues/{project} => text', function() {
+      
+      test('No body', function(done) {
+        
+      });
+      
+      test('One field to update', function(done) {
+        
+      });
+      
+      test('Multiple fields to update', function(done) {
+        
+      });
+      
+    });
+    
+    suite('GET /api/issues/{project} => Array of objects with issue data', function() {
+      
+      test('No filter', function(done) {
+        chai.request(server)
+        .get('/api/issues/test')
+        .query({})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+          done();
+        });
+      });
+      
+      test('One filter', function(done) {
+        
+      });
+      
+      test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
+        
+      });
+      
+    });
+    
+    suite('DELETE /api/issues/{project} => text', function() {
+      
+      test('No _id', function(done) {
+        
+      });
+      
+      test('Valid _id', function(done) {
+        
+      });
       
     });
     
   });
   
-  suite('DELETE /api/issues/{project} => text', function() {
-    
-    test('No _id', function(done) {
-      
-    });
-    
-    test('Valid _id', function(done) {
-      
-    });
-    
-  });
-  
-});
